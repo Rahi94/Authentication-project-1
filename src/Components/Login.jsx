3af6689b1c4f08115235305d219fb63ea6d1a114
tasib94 <rahi84974@gmail.com>
2024-07-05 05:57:29 -0600

    login and register page done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
     return (
-        <div>
-           <h3>This is login page!</h3> 
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col ">
+                <div className="text-center p-8">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+
+                </div>
+                <div className="card p-4 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleLogin}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        <div className='mt-4'>
+                            <p>New here? please <Link to='/register' className='text-blue-600'>Register</Link></p>
+                        
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Login;<h3>This is login page!</h3>
+export default Login; <h3>This is login page!</h3>
